ab619ed9f72cf70e8bedb02d4de5086bb1a4c878
rbrav <70482512+rbrav@users.noreply.github.com>
2020-10-12 18:10:57 -0500

    Create R exercise - vector sorting.R

---
+++ R exercise - vector sorting.R
@@ -0,0 +1,8 @@
+myCourses <- c("Calculus 1", "Calculus 2", "Statistics 1", "Quantitative Analysis", "Research Methods", "Physics 1", "Physics 2", "Biochemistry", "Cognitive Psychology")
+length(myCourses)
+myCourses[c(1, 2)]
+myCourses[c(3, 4)]
+sort(myCourses, 
+     method = "radix")
+sort(myCourses, decreasing = TRUE,
+     method = "radix")
